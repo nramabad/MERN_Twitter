6a99c7c7d93f4d8dd307da8cc5a42f5bd0a1c4fb
nramabad <nramabad@gmail.com>
2018-12-24 18:03:16 -0800

    Add basic user auth files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
-const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -15,11 +14,26 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-
-app.get("/", (req, res) => res.send("Hello World"));
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.get("/", (req, res) => {
+    const user = new User({
+        handle: 'jim',
+        email: 'jim@jim.jim',
+        password: 'jimothy'
+    })
+    user.save();
+    res.send("Hello World!");
+});
+
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
