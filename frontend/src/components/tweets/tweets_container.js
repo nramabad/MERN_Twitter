810d61e75b5cc45d635dbbf92a7ed40fa2de3255
nramabad <nramabad@gmail.com>
2018-12-29 23:58:12 -0800

    Add tweet functionality

---
+++ frontend/src/components/tweets/tweets_container.js
@@ -0,0 +1,19 @@
+// src/components/tweets/tweets_container.js
+
+import { connect } from 'react-redux';
+import { fetchTweets } from '../../actions/tweet_actions';
+import Tweets from './tweets';
+
+const mapStateToProps = (state) => {
+    return {
+        tweets: Object.values(state.tweets.all)
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchTweets: () => dispatch(fetchTweets())
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Tweets);
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -3,11 +3,12 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
-// import tweets from './tweets_reducer';
+import tweets from './tweets_reducer';
 
 const RootReducer = combineReducers({
     errors,
-    session
+    session,
+    tweets
 });
 
 export default RootReducer;
