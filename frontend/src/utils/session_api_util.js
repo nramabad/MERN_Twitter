489589e24ce6ce5728c10013daa7efdd7bc5b88d
nramabad <nramabad@gmail.com>
2018-12-29 22:52:11 -0800

    Add frontend directories and auth

---
+++ frontend/src/utils/session_api_util.js
@@ -0,0 +1,19 @@
+// src/util/session_api_util.js
+
+import axios from 'axios';
+
+export const setAuthToken = token => {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = token;
+    } else {
+        delete axios.defaults.headers.common['Authorization'];
+    }
+};
+
+export const signup = (userData) => {
+    return axios.post('/api/users/register', userData);
+};
+
+export const login = (userData) => {
+    return axios.post('/api/users/login', userData);
+};
