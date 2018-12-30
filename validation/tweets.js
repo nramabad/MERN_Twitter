e887f5d494126ef21f0b5c2ac95f5ebd9dfc599c
nramabad <nramabad@gmail.com>
2018-12-30 00:54:15 -0800

    Attempt heroku fix

--- validation/tweets.js
+++ validation/tweets.js
@@ -2,20 +2,20 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 
 module.exports = function validateTweetInput(data) {
-  let errors = {};
+    let errors = {};
 
-  data.text = validText(data.text) ? data.text : '';
+    data.text = validText(data.text) ? data.text : '';
 
-  if (!Validator.isLength(data.text, { min: 5, max: 140 })) {
-    errors.text = 'Tweet must be between 5 and 140 characters';
-  }
+    if (!Validator.isLength(data.text, { min: 5, max: 140 })) {
+        errors.text = 'Tweet must be between 5 and 140 characters';
+    }
 
-  if (Validator.isEmpty(data.text)) {
-    errors.text = 'Text field is required';
-  }
+    if (Validator.isEmpty(data.text)) {
+        errors.text = 'Text field is required';
+    }
 
-  return {
-    errors,
-    isValid: Object.keys(errors).length === 0
-  };
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    };
 };
